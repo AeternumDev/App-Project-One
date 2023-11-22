ef4b4c70c9405b00bbe2d4c2b9c05c33f95ef34f
Antonio <126622006+AeternumDev@users.noreply.github.com>
2023-11-22 19:59:22 +0100

    Version 1.6.0-alpha

--- app/src/main/java/com/example/taskmaker/CustomCalender.kt
+++ app/src/main/java/com/example/taskmaker/CustomCalender.kt
@@ -3,30 +3,29 @@ package com.example.taskmaker
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.SizeTransform
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.with
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -37,6 +36,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,7 +48,6 @@ import java.util.*
 
 
 
-
 // This annotation specifies that the following function requires a minimum API level of 34 to work.
 @RequiresApi(34)
 // This annotation marks a function as a Composable, which means it can define UI elements in Jetpack Compose.
@@ -56,21 +55,16 @@ import java.util.*
 // enables Composable preview
 @Composable
 fun CustomCalendar() {
-    // This variable holds the current month, and 'remember' is used to retain its value across recompositions.
     var currentMonth by remember { mutableStateOf(YearMonth.now()) }
-    // This calculates the days to be displayed in the current month.
-    val daysInMonth = getDaysInMonth(currentMonth)
 
-    // Column is a Composable that places its children vertically.
     Column {
-        // MonthNavigation is a custom Composable function defined below to navigate between months.
         MonthNavigation(currentMonth) { direction ->
-            // Changes the current month when navigation buttons are clicked.
             currentMonth = currentMonth.plusMonths(direction.toLong())
         }
 
-        // CalendarGrid is a custom Composable function defined below that displays the days of the month in a grid.
-        CalendarGrid(currentMonth)
+        CalendarGrid(currentMonth) { direction ->
+            currentMonth = currentMonth.plusMonths(direction.toLong())
+        }
     }
 }
 
@@ -92,20 +86,20 @@ fun MonthNavigation(currentMonth: YearMonth, onMonthChange: (Int) -> Unit) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         StyledButton(onClick = { onMonthChange(-1) }) {
-            Text("◀", fontSize = 20.sp)
+            Text("<", fontSize = 15.sp)
         }
 
         Text(
             text = currentMonth.format(formatter),
             color = textColor,
-            fontSize = 20.sp,
+            fontSize = 16.sp,
             modifier = Modifier
                 .weight(1f)
                 .wrapContentWidth(Alignment.CenterHorizontally)
         )
 
         StyledButton(onClick = { onMonthChange(1) }) {
-            Text("▶", fontSize = 20.sp)
+            Text(">", fontSize = 15.sp)
         }
     }
 }
@@ -115,16 +109,15 @@ fun StyledButton(onClick: () -> Unit, content: @Composable () -> Unit) {
     Button(
         onClick = onClick,
         shape = RoundedCornerShape(10.dp),
-        modifier = Modifier.size(50.dp),
+        modifier = Modifier.size(40.dp),
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color(0xFF5F30DB),
-            contentColor = Color.White
+            backgroundColor = Color.White,
+            contentColor = Color.Black
         ),
         elevation = ButtonDefaults.elevation(
             defaultElevation = 4.dp,
             pressedElevation = 6.dp
-        ),
-        border = BorderStroke(1.dp, Color(0xFF5F30DB))
+        )
     ) {
         content()
     }
@@ -163,85 +156,144 @@ fun getDaysInMonth(currentMonth: YearMonth): List<LocalDate> {
 @RequiresApi(34)
 @OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
 @Composable
-fun CalendarGrid(month: YearMonth) {
+fun CalendarGrid(month: YearMonth, onMonthChange: (Int) -> Unit) {
     val daysInMonth = getDaysInMonth(month)
     var selectedDate by remember { mutableStateOf<LocalDate?>(null) }
+    val dragAmount = remember { mutableStateOf(0f) }
 
-    // Determine the text color based on the theme
     val textColor = if (isSystemInDarkTheme()) {
-        Color.White // Use white color in dark theme
+        Color.White
     } else {
-        MaterialTheme.colors.onBackground // Use default color in light theme
+        MaterialTheme.colors.onBackground
     }
 
-    // Updated AnimatedContent with only slide animation
+    val dragGesture = Modifier.draggable(
+        orientation = Orientation.Horizontal,
+        state = rememberDraggableState { delta ->
+            dragAmount.value += delta
+        },
+        onDragStopped = {
+            if (Math.abs(dragAmount.value) > 100) { // Adjust the threshold based on your needs
+                if (dragAmount.value > 0) {
+                    onMonthChange(-1)
+                } else {
+                    onMonthChange(1)
+                }
+            }
+            dragAmount.value = 0f
+        }
+    )
     AnimatedContent(
         targetState = month,
+        modifier = dragGesture,
         transitionSpec = {
-            (slideInHorizontally(initialOffsetX = { 300 }) + fadeIn() with
-                    slideOutHorizontally(targetOffsetX = { -300 })+ fadeOut(animationSpec = tween(durationMillis = 150))) // Slide out to the left
-                .using(SizeTransform(clip = false))
+            // Determine the direction of the month change
+            val direction = if (targetState > initialState) 1 else -1
+
+            // Configure slide in and out animations based on direction
+            slideInHorizontally(initialOffsetX = { direction * it }) + fadeIn() with
+                    slideOutHorizontally(targetOffsetX = { -direction * it }) + fadeOut()
         }, label = ""
     ) { targetMonth ->
-        val daysInTargetMonth = getDaysInMonth(targetMonth) // Calculate days for the target month
+        val daysInTargetMonth = getDaysInMonth(targetMonth)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth() // Fill the maximum width
-                .padding(horizontal = 20.dp), // Adjust horizontal padding for centering
-            contentAlignment = Alignment.Center // Align the content to the center
-        ) {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(7), // 7 columns for 7 days
-                contentPadding = PaddingValues(5.dp) // Padding around the grid
+        Column {
+            // Display weekdays
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                items(daysInMonth) { day ->
-                    Box(
-                        modifier = Modifier
-                            .size(40.dp) // Fixed size for each cell
-                            .background(
-                                if (day == selectedDate) MaterialTheme.colors.primary else Color.Transparent,
-                                CircleShape
-                            )
-                            .clickable { selectedDate = day }
-                            .padding(8.dp), // Padding inside each cell
-                        contentAlignment = Alignment.Center
-                    ) {
+                Spacer(Modifier.width(40.dp)) // Space for week numbers
+                val weekdays = listOf("M", "T", "W", "T", "F", "S", "S")
+                val orderedWeekdays = weekdays.rotate(firstDayOfWeek.ordinal)
+
+                orderedWeekdays.forEach { day ->
+                    Text(text = day, textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(end = 25.dp))
+
+
+                }
+            }
+
+            // Custom grid for calendar
+            Row {
+                // Column for week numbers
+                Column(
+                    modifier = Modifier.padding(horizontal = 15.dp)
+                ) {
+                    daysInTargetMonth.chunked(7).forEach { week ->
                         Text(
-                            text = day.dayOfMonth.toString(),
-                            color = if (day == selectedDate) MaterialTheme.colors.onSecondary else textColor,
-                            fontSize = 15.sp
+                            text = "W" + week.first()
+                                .get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear())
+                                .toString(),
+                            modifier = Modifier
+                                .height(40.dp) // Match the height of day cells
+                                .align(Alignment.CenterHorizontally)// Fixed width
                         )
+                    }
+                }
+
+                // Calendar days grid
+                Column(
+                    modifier = Modifier
+                        .padding(end = 25.dp)
+
+
+                ) {
+                    daysInTargetMonth.chunked(7).forEachIndexed { index, week ->
+                        Row {
+                            // Add spacers if the first week
+                            if (index == 0) {
+                                repeat(week.first().dayOfWeek.ordinal - firstDayOfWeek.ordinal) {
+                                    Spacer(modifier = Modifier
+                                        .weight(1f))
+                                }
+                            }
+
+                            week.forEach { day ->
+                                Box(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .size(40.dp)
+                                        .padding(0.dp)
+                                        .background(
+                                            if (day == selectedDate) MaterialTheme.colors.primary else Color.Transparent,
+                                            CircleShape
+                                        )
+                                        .clickable { selectedDate = day }
+                                        .padding(8.dp),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = day.dayOfMonth.toString(),
+                                        color = if (day == selectedDate) MaterialTheme.colors.onSecondary else textColor,
+                                        fontSize = 12.sp
+                                    )
+                                }
+                            }
+
+                            // Add spacers if the last week
+                            if (index == daysInTargetMonth.size / 7 - 1) {
+                                repeat((7 - week.size) % 7) {
+                                    Spacer(modifier = Modifier.weight(1f))
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-
-        // CALCULATION OF DAYS OF THE MONTH
-        @RequiresApi(34)
-        fun getDaysInMonth(currentMonth: YearMonth) {
-            // Calculate the first day to be displayed (might be in the previous month).
-            val firstOfMonth = currentMonth.atDay(1)
-            val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
-            var firstVisibleDay = firstOfMonth
-            while (firstVisibleDay.dayOfWeek != firstDayOfWeek) {
-                firstVisibleDay = firstVisibleDay.minusDays(1)
-            }
-
-            // Calculate the last day to be displayed (might be in the next month).
-            val lastOfMonth = currentMonth.atEndOfMonth()
-            var lastVisibleDay = lastOfMonth
-            while (lastVisibleDay.dayOfWeek != firstDayOfWeek.minus(1))
-                lastVisibleDay = lastVisibleDay.plusDays(1)
-        }
-
     }
+
 }
 
+fun <T> List<T>.rotate(distance: Int): List<T> {
+val listSize = this.size
+val actualDistance = distance % listSize
+return this.drop(actualDistance) + this.take(actualDistance)}
 
-// Return the list of days from the first to the last visible day.
 
-
-
-// Return the list of days from the first to the last visible day.
 
--- app/src/main/java/com/example/taskmaker/MainActivity.kt
+++ app/src/main/java/com/example/taskmaker/MainActivity.kt
@@ -6,17 +6,25 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.drawerlayout.widget.DrawerLayout
@@ -24,7 +32,7 @@ import com.aeternumindustries.taskmaker.R
 import com.aeternumindustries.taskmaker.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-
+import kotlinx.coroutines.delay
 
 // Import statements: These help the code understand where to get certain tools and definitions
 // They referencing a dictionary to understand the meaning of a word.
@@ -41,6 +49,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var navigationView: NavigationView
+    private var showAddTaskDialog by mutableStateOf(false)
+
 
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,13 +60,14 @@ class MainActivity : AppCompatActivity() {
         val sharedPref = getSharedPreferences("task_preferences", Context.MODE_PRIVATE)
         val savedTasks = sharedPref.getStringSet("tasks", null)
         tasks.addAll(savedTasks ?: listOf("Apfel", "Birne")) // Load saved tasks or default ones
+        showAddTaskDialog = false
 
         // Inflate the layout defined in XML and set it as the content view
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Initialize UI components by finding them in the layout
-        fab = findViewById(R.id.floatingActionButton)
+
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.nav_view)
         toggle = ActionBarDrawerToggle(this, drawerLayout, binding.materialToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -65,41 +76,80 @@ class MainActivity : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        // Set an onClickListener on the FloatingActionButton to add new tasks
-        fab.setOnClickListener {
-            // (Your existing code for adding a task would go here)
-        }
-
         // Set the Compose UI content within the designated ComposeView in the XML layout
         binding.composeView.setContent {
-            TaskListScreen(tasks) // This calls a Composable function to render the task list
+            TaskListScreen(tasks)
+            if (showAddTaskDialog) {
+                AddTaskDialog(onAdd = { task ->
+                    tasks.add(task)
+                    showAddTaskDialog = false
+                    saveTasks()
+                }, onDismiss = {
+                    showAddTaskDialog = false
+                })
+            }
         }
     }
 
     // This Composable function defines the UI layout for the task list screen
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewTaskListScreen() {
+        val tasks = remember { mutableStateListOf("Task 1", "Task 2", "Task 3") }
+        TaskListScreen(tasks = tasks)
+    }
 
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     @Composable
     fun TaskListScreen(tasks: SnapshotStateList<String>) {
-        // A Column arranges its children vertically
-        Column(modifier = Modifier.fillMaxWidth()) {
-            CustomCalendar() // This is a custom Composable for showing a calendar
-            TaskList(tasks) // Composable function that displays the task list
-        }
+        // Scaffold is used to provide material design components
+        Scaffold(
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { showAddTaskDialog = true },
+                    shape = RoundedCornerShape(16.dp),
+                    backgroundColor = Color(0xFF5F30DB),
+                    modifier = Modifier
+                        .padding(horizontal = 5.dp, vertical = 80.dp)) {
+                    Icon(
+                        Icons.Default.Add, "Floating action button.",
+                        tint = Color.White)
+                }
+            },
+            content = { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(paddingValues)
+                ) {
+                    CustomCalendar() // This is a custom Composable for showing a calendar
+                    TaskList(tasks) // Composable function that displays the task list
+                }
+            }
+        )
     }
-
     // This Composable function displays the tasks in a LazyColumn (like RecyclerView)
+
     @Composable
     fun TaskList(tasks: SnapshotStateList<String>) {
         LazyColumn(
             modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp,)
+                .padding(start = 10.dp, end = 10.dp)
                 .padding(horizontal = 8.dp)
                 .padding(vertical = 10.dp)
         ) {
-            // Display each task in the list with TaskItem Composable
             items(items = tasks, key = { it }) { task ->
-                TaskItem(task = task)
+                var isVisible = remember { mutableStateOf(true) }
+
+                TaskItem(task = task, isVisible = isVisible, onDelete = {
+                    if (isVisible.value) { // Use isVisible.value instead of just isVisible
+                        isVisible.value = false // Trigger exit animation
+                    } else {
+                        tasks.remove(task) // Remove the task after animation
+                    }
+                })
+
                 Divider(color = Color.LightGray, thickness = 1.dp)
             }
         }
@@ -107,43 +157,95 @@ class MainActivity : AppCompatActivity() {
 
     // Defines the UI for each task item in the list
     @Composable
-    fun TaskItem(task: String) {
-        val textColor = if (isSystemInDarkTheme()) {
-            Color.White // Use white color in dark theme
-        } else {
-            MaterialTheme.colors.onBackground // Use default color in light theme
-        }
-        // A Row arranges its children horizontally
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 3.dp, horizontal = 0.dp)
+    fun TaskItem(task: String, onDelete: (String) -> Unit, isVisible: MutableState<Boolean>) {
+        val isVisibleValue = isVisible.value // Extract the value of isVisible
+
+        AnimatedVisibility(
+            visible = isVisibleValue, // Pass the extracted value to AnimatedVisibility
+            exit = fadeOut(animationSpec = tween(durationMillis = 300)) + shrinkVertically() // Add fade-out and shrink animations for exit
         ) {
-            Text(
-                text = task,
-                color = textColor,
-                fontSize = 16.sp,
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
-
-            Spacer(Modifier.weight(1f)) // Spacer for pushing the delete button to the end
-
-            IconButton(
-                onClick = { /* Handle delete action */ },
+            Row(
                 modifier = Modifier
-                    .align(Alignment.CenterVertically)
-
+                    .animateContentSize() // Animate size changes
+                    .fillMaxWidth()
+                    .padding(vertical = 3.dp, horizontal = 0.dp)
             ) {
-                Icon(
-                    imageVector = Icons.Rounded.Delete,
-                    contentDescription = "Delete",
-                    tint = Color(0xFF5F30DB))
+                // Determine the text color based on the theme
+                val textColor = if (isSystemInDarkTheme()) {
+                    Color.White // Use white color in dark theme
+                } else {
+                    MaterialTheme.colors.onBackground
+                    // Use default color in light theme
+                }
+
+                Text(
+                    text = task,
+                    color = textColor,
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+
+                Spacer(Modifier.weight(1f)) // Spacer for pushing the delete button to the end
+
+                IconButton(
+                    onClick = {
+                        isVisible.value = false // Update visibility
+                        onDelete(task)
+                    },
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Delete,
+                        contentDescription = "Delete",
+                        tint = Color(0xFF5F30DB)
+                    )
+                }
+
+                if (!isVisibleValue) { // Use isVisibleValue instead of isVisible
+                    LaunchedEffect(task) {
+                        delay(300) // Duration for exit animation
+                        onDelete(task)
+                    }
+                }
             }
         }
     }
 
+
+    @Composable
+    fun AddTaskDialog(onAdd: (String) -> Unit, onDismiss: () -> Unit) {
+        var text by remember { mutableStateOf("") }
+
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            title = { Text("Add Task") },
+            text = {
+                TextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Task Name") }
+                )
+            },
+            confirmButton = {
+                Button(onClick = { onAdd(text) }) {
+                    Text("Add")
+                }
+            },
+            dismissButton = {
+                Button(onClick = {
+                    // Check if a task has been added
+                    text = "" // Reset the text field
+                    showAddTaskDialog = true // Keep the dialog open
+                }) {
+                    Text("Cancel")
+                }
+            }
+
+
+        )
+    }
     // Function to save the current state of tasks to SharedPreferences
-    fun saveTasks() {
+    private fun saveTasks() {
         val sharedPref = getSharedPreferences("task_preferences", Context.MODE_PRIVATE)
         with(sharedPref.edit()) {
             putStringSet("tasks", tasks.toSet()) // Convert the task list to a set and save it
